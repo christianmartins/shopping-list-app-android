239623d35096ba9ca573bde32cc044b4ad72a801
christianmartins <christianrmmartins@gmail.com>
2020-01-22 23:36:25 -0300

    ADD: adding url heroku

--- app/src/main/java/br/com/shoppinglistapp/data/webservice/NetworkServiceProvider.kt
+++ app/src/main/java/br/com/shoppinglistapp/data/webservice/NetworkServiceProvider.kt
@@ -1,9 +1,6 @@
 package br.com.shoppinglistapp.data.webservice
 
-import br.com.shoppinglistapp.App
-import br.com.shoppinglistapp.BuildConfig
-import br.com.shoppinglistapp.R
-import br.com.shoppinglistapp.extensions.nonNullable
+import br.com.shoppinglistapp.utils.GlobalUtils
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -12,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class NetworkServiceProvider {
 
     fun getService(): ShoppingListAppApi {
-        val url = App.context?.getString(if (BuildConfig.DEBUG) R.string.url_debug else R.string.url_release).nonNullable()
+        val url = GlobalUtils.urlApp
         val gson = GsonBuilder().setLenient().create()
         val retrofit = Retrofit.Builder()
             .baseUrl(url)
--- app/src/main/java/br/com/shoppinglistapp/utils/GlobalUtils.kt
+++ app/src/main/java/br/com/shoppinglistapp/utils/GlobalUtils.kt
@@ -1,7 +1,10 @@
 package br.com.shoppinglistapp.utils
 
+import br.com.shoppinglistapp.App
+import br.com.shoppinglistapp.R
 import br.com.shoppinglistapp.data.model.ItemShoppingList
 import br.com.shoppinglistapp.data.model.ShoppingList
+import br.com.shoppinglistapp.extensions.nonNullable
 
 object GlobalUtils {
 
@@ -9,6 +12,7 @@ object GlobalUtils {
     val shoppingLists = ArrayList<ShoppingList>()
     val itemsShoppingList = ArrayList<ItemShoppingList>()
     var fragmentAlive = ""
+    val urlApp = App.context?.getString(R.string.url_app).nonNullable()
 
     fun clearLists(){
         shoppingLists.clear()
